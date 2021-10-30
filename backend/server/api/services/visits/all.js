ed48206ebfdce3a663541968ab8c251c3538251e
Joshua Santos <JoshCSan@gmail.com>
2021-10-30 09:19:39 -0700

    starting visit

--- backend/server/api/services/visits/all.js
+++ backend/server/api/services/visits/all.js
@@ -2,7 +2,23 @@ const SQL = require('sql-template-strings');
 const { queryRows } = require('../helpers');
 
 const all = async () => {
-  const query = SQL`SELECT * FROM "Visits"`;
+  const query = SQL`
+  SELECT "Visits".user_id,
+    "Visits".restaurant_id,
+    "Visits".name,
+    "Restaurants".image_url "Restaurants.image_url",
+    "Restaurants".location "Restaurants.location",
+    "Restaurants".category "Restaurants.category",
+    "Restaurants".price "Restaurants.price",
+    "Restaurants".rating "Restaurants.rating",
+    "Restaurants".review_count "Restaurants.review_count"
+
+  FROM "Visits" "Visits"
+    LEFT OUTER JOIN "Restaurants" "Restaurants" ON "Restaurants"._id = "Visits".restaurant_id
+  ORDER BY
+    "Visits".visited_at DESC
+  `;
+
   const data = await queryRows(query);
   return {
     total: data.length,
--- backend/server/api/services/visits/router.js
+++ backend/server/api/services/visits/router.js
@@ -2,7 +2,7 @@ const controller = require('./controller');
 
 const routes = (router, prefix) => {
   // get all
-  router.get(`${prefix}/all`, controller.all);
+  router.get(`${prefix}/`, controller.all);
 };
 
 module.exports = routes;
